f351e4f5444340433583a6d3a5c5c9e00b16f447
Jack <jpnortman27@gmail.com>
2020-08-30 16:05:42 -0500

    Camera movement added

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _objectToFollow = null;
+
+    Vector3 _objectOffset;
+
+    private void Awake()
+    {
+        // create an offset and keep that offset
+        _objectOffset = this.transform.position - _objectToFollow.position;
+    }
+
+    // happens after update so that camera always moves last
+    private void LateUpdate()
+    {
+        // apply the offset every frame
+        this.transform.position = _objectToFollow.position + _objectOffset;
+    }
+}
